2e08624169bcd55e211f70508154fd98cc971a82
Brian Mapes <mapes@miami.edu>
2024-09-11 13:15:01 -0400

    tinkering in matlab

--- CauchyWaveletScript.m
+++ CauchyWaveletScript.m
@@ -1,12 +1,20 @@
 % Import video 
-v = VideoReader("Baja_202307051401-202307052331_g18_conus_band2_vonkarmans-waves_nolabels.mp4");
+%v = VideoReader("DATA/Baja_202307051401-202307052331_g18_conus_band2_vonkarmans-waves_nolabels.mp4");
+
+v = VideoReader("DATA/syn_scwave_warp_modx5.mp4");
+
 video = read(v);
 
 % red channel brightness only 
 red = squeeze( video(:,:,1,:) );
 % size(red) 1080, 1920, 115
 
-frame50=red(:,:,50);
+%%% SELECT ONE FRAME FOR SPATIAL ANALYSIS
+% First argument is y increasing downward, second is x from left
+% frame50=red(:,:,50); % for vonkarmans case
+frame50=red(100:700,150:880,3); % for synthetic case
+
+% QUICK SHOW 
 figure(1)
 imshow(frame50); colorbar;
 
@@ -14,31 +22,26 @@ imshow(frame50); colorbar;
 
 %%% Wavelet transform 
 
-% a set of 8 Angles from 0 to pi 
-Angles = 0:pi/8:pi-pi/8
+% a set of 7 Angles from 0 to pi (N, NNW, WNW, W, WSW, SSW, )
+Angles = 0:pi/12:pi
 
-% a set of 10 Scales
-Scales = 5:5:50
+% a LOGARITHMIC set of 10 Scales
+% Scales = [2,5,10,20,40,80,160,320,640,1000] % 2,5,10 too small, 1000 too big
+% Better range 
+Scales = 10.^(1.2:.2:3);
 
 cwtCauchy = cwtft2(frame50,wavelet="cauchy",scales=Scales, angles=Angles);
 
 spec = squeeze( cwtCauchy.cfs );
-size(spec) 
 
 % RESULT: size 1080,1920, 10 SCALES, 8 ANGLES
 
 
-% Overlay wavelet power on image 
-figure(1)
-imshow(frame50); colorbar; axis on
+% Amplitude seems to grow roughly linearly with scale. 
+% figure(2); plot( Scales,squeeze(abs(spec(500,500,:,7))) )
 
-hold on
+% function image_with_wavelet_overlay(spec, Scales, scale, angle)
 
-posLevels = 10:20:90; negLevels = -90:20:-10;
+image_with_wavelet_overlay(frame50, spec, Scales, 3, 6);
 
-% Real part is crests and trofs, imag is gradients, abs is a magnitude map 
-contour( real(spec(:,:,10,7)), LevelList=posLevels,EdgeColor='red' );
-contour( real(spec(:,:,10,7)), LevelList=negLevels,EdgeColor='blue' );
-
-% "power" is amplitude abs() squared 
-contour( abs(spec(:,:,10,7)).^2 );
+power = abs(spec) .^2;
---
+++ image_with_wavelet_overlay.m
@@ -0,0 +1,24 @@
+
+function output = image_with_wavelet_overlay(img,spec, Scales, scale, angle)
+    % Overlay wavelet power on image 
+    figure(1)
+    imshow(img); colorbar; axis on
+    
+    hold on
+
+    posLevels = 1:2:9;
+    negLevels = -9:2:-1;
+
+    % Adjust contour levels by Scale as factor (for real/imag), factor^2 (for power)
+    factor = Scales(scale) 
+    
+    % Real part is crests and trofs, imag is gradients, abs is a magnitude map 
+    contour( real(spec(:,:,scale,angle)), LevelList=posLevels*factor, EdgeColor='red' );
+    contour( real(spec(:,:,scale,angle)), LevelList=negLevels*factor, EdgeColor='blue' );
+    
+    % "power" is amplitude abs() squared 
+    contour( (abs(spec(:,:,scale,angle))*factor) .^2 );
+
+    % legend
+end
+
